d4e50384c8c1ec5946b97b42f35a763a622b630a
“MorpheusEpsilon” <joseantoniomurphy@icloud.com>
2025-05-16 17:29:47 -0400

    Add motivation route and user auth system

--- app/main.py
+++ app/main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI #Imports  fastAPI to start the code
 
 app = FastAPI() #Creates the controller, the core of the application
 
-@app.get("/") #This is the route for the homepage
+@app.get("/") #This is the route for the homepage, traducing the code to FastAPI
 #When someone goes to the homepage (/), show them this message
 def read_root(): 
     return {"message": "Welcome to Momentum!"}  #This is what gets shown on the homepage
+
+@app.get("/motivation") #routhe for user page
+def motivation():
+    return 0
---
+++ app/motivation.py
@@ -0,0 +1,10 @@
+import fastapi
+from fastapi import APIRouter
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+router = APIRouter()
